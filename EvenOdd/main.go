f3ef49d9f702f7d8dfa88be2ac25680b27b07c9e
Jono Echols <jono.echols@visioncritical.com>
2018-11-12 18:35:31 -0800

    even odd assigmnet

---
+++ EvenOdd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	theInts := []int{0: 10}
+
+	for i := range theInts {
+		if i%2 == 0 {
+			fmt.Println(i, " is even")
+		} else {
+			fmt.Println(i, " is odd")
+		}
+	}
+}
